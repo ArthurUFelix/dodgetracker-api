0e8c4afa8414d65338f3c2c6f0a377c7e491e0d0
Arthur Felix <arthurufelix@gmail.com>
2024-09-05 08:11:35 -0300

    Fix migration

--- knex/migrations/20240903115249_dodgetracker.js
+++ knex/migrations/20240903115249_dodgetracker.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export const up = function(knex) {
   return knex.schema.createTable('dodges', (table) => {
     table.increments('id')
     table.string('summonerId')
@@ -20,6 +20,6 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+export const down = function(knex) {
   
 };
--- knex/migrations/20240903201954_alter_dodges.js
+++ knex/migrations/20240903201954_alter_dodges.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+export const up = function(knex) {
   return knex.schema.alterTable('dodges', (table) => {
     table.enu('queue', ['SOLO', 'FLEX']);
   })
@@ -12,6 +12,6 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+export const down = function(knex) {
   
 };
